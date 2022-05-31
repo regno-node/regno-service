55e138c5eb665644ad506bfdd6a17714641528da
Nurviro <nurviro@protonmail.com>
2022-05-30 22:56:52 -0400

    add sorting to peer table & use get_connections instead of sync_info

--- lib/regno_web/live/monero_sync_info_live.ex
+++ lib/regno_web/live/monero_sync_info_live.ex
@@ -1,34 +1,35 @@
-defmodule RegnoWeb.MonerodSyncInfoView do
+defmodule RegnoWeb.MonerodConnectionsView do
   use RegnoWeb, :live_view
   require Logger
+  alias Phoenix.LiveView.JS
 
   def render(assigns) do
     ~H"""
     <h2 class="text-lg font-bold mb-3 mt-10">Connected peers</h2>
-    <%= if assigns[:sync_info] do %>
+    <%= if assigns[:connections] do %>
     <div class="overflow-x-auto border-x border-t rounded-sm">
-    <table class="table-auto w-full">
+    <table class="table-auto text-sm w-full">
     <thead class="border-b">
     <tr class="bg-gray-100 dark:bg-slate-700">
-      <th class="text-left p-4 font-medium">Peer ID</th>
-      <th class="text-left p-4 font-medium">Host:port</th>
-      <th class="text-left p-4 font-medium">Block height</th>
-      <th class="text-left p-4 font-medium">Incoming?</th>
-      <th class="text-left p-4 font-medium">Live time</th>
-      <th class="text-left p-4 font-medium">Send idle time</th>
-      <th class="text-left p-4 font-medium">Recv idle time</th>
+      <th phx-click={JS.push("sort", value: %{sort_key: "peer_id"})} class="text-left p-4 font-medium">Peer ID</th>
+      <th phx-click={JS.push("sort", value: %{sort_key: "address"})} class="text-left p-4 font-medium">Host:port</th>
+      <th phx-click={JS.push("sort", value: %{sort_key: "height"})} class="text-left p-4 font-medium">Block height</th>
+      <th phx-click={JS.push("sort", value: %{sort_key: "incoming"})} class="text-left p-4 font-medium">Incoming?</th>
+      <th phx-click={JS.push("sort", value: %{sort_key: "live_time"})} class="text-left p-4 font-medium">Live time</th>
+      <th phx-click={JS.push("sort", value: %{sort_key: "send_idle_time"})} class="text-left p-4 font-medium">Send idle time</th>
+      <th phx-click={JS.push("sort", value: %{sort_key: "recv_idle_time"})} class="text-left p-4 font-medium">Recv idle time</th>
     </tr>
     </thead>
     <tbody>
-      <%= for peer <- @sync_info["peers"] do %>
+      <%= for peer <- @connections do %>
         <tr class="border-b hover:bg-gray-50 dark:hover:bg-zinc-700">
-          <td class="p-3"><%= peer["info"]["peer_id"] %></td>
-          <td class="p-3"><%= peer["info"]["address"] %></td>
-          <td class="p-3"><%= peer["info"]["height"] %></td>
-          <td class="p-3"><%= peer["info"]["incoming"] %></td>
-          <td class="p-3"><%= peer["info"]["live_time"] %></td>
-          <td class="p-3"><%= peer["info"]["send_idle_time"] %></td>
-          <td class="p-3"><%= peer["info"]["recv_idle_time"] %></td>
+          <td class="p-3"><%= peer["peer_id"] %></td>
+          <td class="p-3"><%= peer["address"] %></td>
+          <td class="p-3"><%= peer["height"] %></td>
+          <td class="p-3"><%= peer["incoming"] %></td>
+          <td class="p-3"><%= peer["live_time"] %></td>
+          <td class="p-3"><%= peer["send_idle_time"] %></td>
+          <td class="p-3"><%= peer["recv_idle_time"] %></td>
         </tr>
       <% end %>
     </tbody>
@@ -47,37 +48,55 @@ defmodule RegnoWeb.MonerodSyncInfoView do
 
     Logger.info("MonerodSyncInfoView: mount")
 
-    case get_sync_info() do
-      {:ok, sync_info} ->
-        {:ok, assign(socket, :sync_info, sync_info)}
-
-      {:error, reason} ->
-        {:ok, put_flash(socket, :error, reason)}
-    end
+    {:ok,
+     socket
+     |> assign(:sort_key, "live_time")
+     |> assign(:sort_dir, :desc)
+     |> get_connections()}
   end
 
   def handle_info(:update, socket) do
     Process.send_after(self(), :update, 5000)
     Logger.info("MonerodSyncInfoView: handle_info")
-
-    case get_sync_info() do
-      {:ok, sync_info} ->
-        {:noreply, assign(socket, :sync_info, sync_info)}
-
-      {:error, reason} ->
-        {:noreply, put_flash(socket, :error, reason)}
-    end
+    {:noreply, get_connections(socket)}
   end
 
-  def get_sync_info() do
-    case Monero.Daemon.sync_info() |> Monero.request() do
-      {:ok, infos} ->
-        {:ok, infos}
+  def get_connections(socket) do
+    case Monero.Daemon.get_connections() |> Monero.request() do
+      {:ok, result} ->
+        assign(
+          socket,
+          :connections,
+          Enum.sort_by(
+            result["connections"],
+            fn peer -> peer[socket.assigns.sort_key] end,
+            socket.assigns.sort_dir
+          )
+        )
 
       {:error, reason} ->
-        error = "Failed to get sync_info: #{reason}"
+        error = "Failed to get connections: #{reason}"
         Logger.error(error)
-        {:error, error}
+        put_flash(socket, :error, reason)
     end
   end
+
+  def handle_event("sort", %{"sort_key" => value}, socket) do
+    if socket.assigns.sort_key == value do
+      Logger.info("sort dir")
+      {:noreply,
+       socket
+       |> assign(:sort_dir, toggle_sort_dir(socket.assigns.sort_dir))
+       |> get_connections()
+       }
+    else
+      {:noreply,
+       socket
+       |> assign(:sort_key, value)
+       |> get_connections()}
+    end
+  end
+
+  def toggle_sort_dir(:desc), do: :asc
+  def toggle_sort_dir(:asc), do: :desc
 end
